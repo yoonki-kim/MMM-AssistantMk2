76231b51bfae1acecb648e9c27a4d511f3af554f
Seongnoh Sean Yi <eouia0819@gmail.com>
2019-02-13 09:26:20 +0100

    return to previous version.

--- node_helper.js
+++ node_helper.js
@@ -21,7 +21,7 @@ module.exports = NodeHelper.create({
   },
 
   play: function(file, command, option, cb=()=>{}) {
-    const player = require('play-sound')({player : command})
+    const player = require('play-sound')()
     this.sendSocketNotification("SPEAKER_ON")
     var co = {}
     co[command] = option
