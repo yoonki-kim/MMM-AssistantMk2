f574b8427c6dacfccf7b961a2275c5e88dfe65bd
bugsounet <github@bugsnana.fr>
2020-01-18 11:44:14 +0100

    upgrade MMM-NewPIR -- vocal control turn off screen

--- recipes/with-MMM-NewPIR.js
+++ recipes/with-MMM-NewPIR.js
@@ -1,15 +1,33 @@
-/**  MMM-NewPIR V2 commands addon  **/
-/**    automatic Turn on screen    **/
-/**       on AMk2 usage            **/
-/**         @bugsounet             **/
+/**  MMM-NewPIR V2 commands addon             **/
+/**    automatic Turn on screen               **/
+/**       on AMk2 usage                       **/
+/**  automatic Turn off after delay time      **/
+/**  Force Turn off vocal control             **/
+/**  modify pattern in your prefered language **/
+/**         @bugsounet                        **/
 
 
 var recipe = {
+  transcriptionHooks: {
+    "NEWPIR_TURNOFF": {
+      pattern: "turn off screen",
+      command: "NEWPIR_TURNOFF"
+    }
+  },
   commands: {
     "NEWPIR_WAKEUP": {
       notificationExec: {
         notification: "USER_PRESENCE",
         payload : true
+      }
+    },
+    "NEWPIR_TURNOFF": {
+      notificationExec: {
+        notification: "USER_PRESENCE",
+        payload : false
+      },
+      soundExec: {
+        chime: "close"
       }
     },
   },
